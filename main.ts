4a936131ec9b3dabec102d1a206b7dc951ddb4c1
poetanoctis <patriciamarshagunardi@gmail.com>
2026-02-20 10:45:27 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,52 @@
+function set_dice_pattern () {
+    myImage = [
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . . . .
+        `)
+    ]
+}
+function roll_dice () {
+    myImage[randint(0, 6)].showImage(0)
+}
+let myImage: Image[] = []
+set_dice_pattern()
+roll_dice()
