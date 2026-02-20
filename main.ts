d51eb44b9d56f02c70dc9d87427cce56d446d68e
poetanoctis <patriciamarshagunardi@gmail.com>
2026-02-20 10:56:29 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -45,8 +45,14 @@ function set_dice_pattern () {
     ]
 }
 function roll_dice () {
-    myImage[randint(0, 6)].showImage(0)
+    myImage[randint(0, 5)].showImage(0)
 }
+input.onGesture(Gesture.Shake, function () {
+    roll_dice()
+    dice_value = randint(0, 6)
+    basic.showNumber(dice_value)
+})
+let dice_value = 0
 let myImage: Image[] = []
 set_dice_pattern()
 roll_dice()
